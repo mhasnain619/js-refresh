d57ed4c064616e6a9402feb1bff34682ee91111f
Muhammad Hasnain <hasnain0893@gmail.com>
2024-10-08 10:37:01 +0500

    qwe

--- app.js
+++ app.js
@@ -1,25 +1,40 @@
-let mydiv = document.querySelector('.mydiv');
+// let mydiv = document.querySelector('.mydiv');
 
-let newBtn = document.createElement('button');
-newBtn.textContent = 'New Button';
+// let newBtn = document.createElement('button');
+// newBtn.textContent = 'New Button';
 
-// newBtn.style.color = 'green'
-mydiv.appendChild(newBtn);
-newBtn.addEventListener('click', function () {
-    newBtn.style.padding = '10px 20px'
-    newBtn.style.margin = '10px'
-    newBtn.style.borderRadius = '10px'
-    newBtn.style.backgroundColor = 'blue'
-    newBtn.style.color = 'white'
+// // newBtn.style.color = 'green'
+// mydiv.appendChild(newBtn);
+// newBtn.addEventListener('click', function () {
+//     newBtn.style.padding = '10px 20px'
+//     newBtn.style.margin = '10px'
+//     newBtn.style.borderRadius = '10px'
+//     newBtn.style.backgroundColor = 'blue'
+//     newBtn.style.color = 'white'
 
-    newBtn.style.border = 'none'
-    mydiv.style.backgroundColor = 'yellow';  // Corrected the typo here
-});
-let newHeading = document.createElement('h1')
-newHeading.textContent = 'New Heading';
-newHeading.style.color = 'aqua'
-newHeading.style.fontSize = '40px'
-newHeading.style.fontWeight = '80px'
-mydiv.before(newHeading)
+//     newBtn.style.border = 'none'
+//     mydiv.style.backgroundColor = 'yellow';  // Corrected the typo here
+// });
+// let newHeading = document.createElement('h1')
+// newHeading.textContent = 'New Heading';
+// newHeading.style.color = 'aqua'
+// newHeading.style.fontSize = '40px'
+// newHeading.style.fontWeight = '80px'
+// mydiv.before(newHeading)
 
 // console.log(newBtn);
+
+let modeChangeBtn = document.querySelector('.modeChange')
+let mode = 'light'
+modeChangeBtn.addEventListener('click', () => {
+    if (mode === 'light') {
+        mode = 'dark'
+        document.body.style.backgroundColor = 'black'
+    } else {
+        mode = 'light'
+        document.body.style.backgroundColor = 'white'
+
+    }
+    console.log(mode);
+
+})
